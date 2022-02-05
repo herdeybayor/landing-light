fb269a6a690ce948e4fd44ca3bff691a01436a7e
Sherifdeen <shereefdeen4real@gmail.com>
2022-02-05 12:50:20 +0100

    UI Completed

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,11 +4,13 @@ import visa from "../assets/visa.png";
 import discover from "../assets/discover.png";
 import paypal from "../assets/paypal.png";
 import VirtualCard from "../components/VirtualCard";
+import CheckoutForm from "../components/CheckoutForm";
+import Hr from "../components/Hr";
 
 function Landing() {
   return (
     <div className="flex flex-col mt-20 px-12">
-      <hr className="border-t border-gray-400" />
+      <Hr />
       <Row>
         <div>
           <h1 className="font-bold text-2xl">Payment Information</h1>
@@ -28,10 +30,30 @@ function Landing() {
       </Row>
       <Row>
         <VirtualCard />
-        <div>
-          <h1>Form</h1>
+        <CheckoutForm />
+      </Row>
+      <Hr />
+      <Row>
+        <div className="font-bold text-xl">
+          <h1>Subtotal</h1>
+          <h1>Estimated TAX</h1>
+          <h1>
+            Promotional Code: <span className="text-gray-400">Z4KXLM9A</span>
+          </h1>
+        </div>
+        <div className="font-bold text-right text-xl mb-10">
+          <h1>&#8358;2,497.00</h1>
+          <h1>&#8358;119.64</h1>
+          <h1>&#8358;-60.00</h1>
         </div>
       </Row>
+      <Hr />
+      <div className="flex justify-between items-center mt-8">
+        <button className="bg-blue-600 submit-button py-4 text-white px-16 font-bold rounded-sm hover:bg-blue-800 transition-colors duration-500">
+          Complete Payment
+        </button>
+        <h1 className="font-extrabold text-2xl">TOTAL: &#8358;2556.64</h1>
+      </div>
     </div>
   );
 }
